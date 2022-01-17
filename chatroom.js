1eb5af065a1641e4ce3139d04eaa29573fa5b457
Sushicodin <87022921+Sushicodin@users.noreply.github.com>
2022-01-17 14:13:32 -0500

    Add files via upload

---
+++ chatroom.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtOraHua3uCCA-wYDFunw-8RGD1sUifhY",
+    authDomain: "chatroomfirebase-817b0.firebaseapp.com",
+    databaseURL: "https://chatroomfirebase-817b0-default-rtdb.firebaseio.com",
+    projectId: "chatroomfirebase-817b0",
+    storageBucket: "chatroomfirebase-817b0.appspot.com",
+    messagingSenderId: "730076600173",
+    appId: "1:730076600173:web:c042945df48ff793aa2c64"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
